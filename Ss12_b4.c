a13f422df6ddb3da1e8862cb280b954933a7a4f9
AnHaiDung <anhaidung2k6@gmail.com>
2024-12-09 20:16:52 +0700

    b4

---
+++ Ss12_b4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void findMaxValue(int arr[],int size){
+	int maxValue= arr[0];
+	for(int i=0; i<size;i++){
+		if(arr[i]>maxValue){
+			maxValue= arr[i];
+		}
+	}
+	printf("phan tu lon nhat trong mang la : %d",maxValue);
+}
+int main(){
+	int numbers[]={4,6,15,9,1,14};
+    int size= sizeof(numbers)/sizeof(int);
+    findMaxValue(numbers,size);
+    return 0;
+}
